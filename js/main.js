a6f1af02601fe3b0027237eb1dcc0f95fac585f0
Angus <angusclark93@gmail.com>
2018-02-26 20:17:31 +0600

    Apply scroll plugin and adapt to content

--- js/main.js
+++ js/main.js
@@ -1,21 +1,46 @@
-let box = $('.box');
-let arrow = $('.expand_arrow');
-arrow.hover(function() {
-  box.addClass('transform_x');
-  arrow.addClass('opacity');
-});
+$(document).ready(function() {
 
-box.mouseleave(function(event) {
-  if (box.hasClass('transform_x')) {
-    box.toggleClass('transform_x')
-    arrow.removeClass('opacity')
-  }
-});
-/*function() {
+  (function() {
 
-});
-/*(function(event) {
+    $('.main').scrollex({
+      mode: 'middle',
+      delay: 50,
+      initialize: function() {
+        $(this).addClass('inactive');
+      },
+      terminate: function() {
+        $(this).removeClass('inactive');
+      },
+      enter: function() {
+        $(this).removeClass('inactive');
+      },
+      leave: function() {
+        $(this).addClass('inactive');
+      }
+    });
 
-  moreLess()
-  bg_mustard()
-});*/
+    let rightArrow = $('.right_arrow');
+    let leftArrow = $('.left_arrow')
+
+
+    rightArrow.hover(function() {
+      $(this).addClass('opacity')
+      $('.box_right').addClass('transform_x');
+    })
+
+    $('.box').mouseleave(function(event) {
+      $('.box_right').removeClass('transform_x');
+      rightArrow.removeClass('opacity')
+    });
+
+    leftArrow.hover(function() {
+      $(this).addClass('opacity')
+      $('#box_right1').addClass('transform_x');
+    })
+
+    $('.box').mouseleave(function(event) {
+      $('#box_right1').removeClass('transform_x');
+      leftArrow.removeClass('opacity')
+    });
+  })();
+});
