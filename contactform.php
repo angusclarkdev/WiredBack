595cad4173b5b86ac973cef8604e4866b391baf4
Angus <angusclark93@gmail.com>
2018-03-08 16:58:25 +0600

    Create PHP for contact form

---
+++ contactform.php
@@ -0,0 +1,23 @@
+<?php
+
+// Get Form Data
+
+if (isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $mailFrom = $_POST['mail'];
+  $message = $_POST['message'];
+
+  $mailTo = 'dingus_clark_@hotmail.co.uk';
+  $headers = "From: ".$mailFrom;
+  $txt = "You have received an email from ".$name.".\n\n".$message;
+
+  mail($mailTo, $txt, $headers);
+  header("Location: index.html?mailsent");
+
+}
+
+
+
+
+
+ ?>
