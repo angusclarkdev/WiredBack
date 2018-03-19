8758df184ee1250ed1eb45b8ef52056e56a69d75
Angus <angusclark93@gmail.com>
2018-03-19 18:59:09 +0600

    fix php form with working RECAPTCHA
    - Add working form
    - Fix php email rendering, make readable
    - Fix contact form sizing for small laptops

--- README.md
+++
@@ -1,5 +0,0 @@
-## WiredBack
-This is the repository for the responsive website I'm currently developing for Wiredback Ltd.
- Currently in development. Expected launch: March 18'.
-### Author:
-Angus Clark
--- form.php
+++ form.php
@@ -1,36 +1,60 @@
 <?php
+error_reporting(-1);
+ini_set('display_errors', 'On');
+set_error_handler("var_dump");
+
+    function post_captcha($user_response) {
+        $fields_string = '';
+        $fields = array(
+            'secret' => '6LdlX00UAAAAAJkIsbWtYKDoJt1tJ8kYb_XOTxIu',
+            'response' => $user_response
+        );
+        foreach($fields as $key=>$value)
+        $fields_string .= $key . '=' . $value . '&';
+        $fields_string = rtrim($fields_string, '&');
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
+        curl_setopt($ch, CURLOPT_POST, count($fields));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, True);
+
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($result, true);
+    }
+
+    // Call the function post_captcha
+    $res = post_captcha($_POST['g-recaptcha-response']);
+
+    if (!$res['success']) {
+        // What happens when the CAPTCHA wasn't checked
+        echo '<p>Please go back and make sure you check the security CAPTCHA box.</p><br>';
+    } else {
+        // If CAPTCHA is successfully completed...
+
+        $name = $_POST['name'];
+        $mailFrom = $_POST['email'];
+        $message = $_POST['message'];
+        $subject = '';
+
+        $mailTo = "garry@wiredback.com";
 
 
-// Conditions for server side validation 
+        $emailBody = "Name: $name.\n \n".
 
-if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['message'])) {
+                      "Email: $mailFrom.\n \n \n \n".
 
-  // Get Form Data
-
-  $name = $_POST['name'];
-  $mailFrom = $_POST['email'];
-  $message = $_POST['message'];
-
-  $mailTo = "garry@wiredback.com";
-
-
-  $emailBody = "Name: $name.\n".
-
-                "Email: $mailFrom.\n".
-
-                    "Message: $message.\n";
+                          "$message.\n";
 
 
 
-  $header = "From: $mailFrom";
-
-  mail($mailTo, $emailBody, $header);
-
-  header("Location: index.php");
-
-} else {
-  echo "Error: All fields are required to have a value";
-}
+        $header = "From: ".$mailTo;
 
 
- ?>
+
+        mail($mailTo, $subject, $emailBody, $header);
+
+        echo '<br><p>Thanks for the inquiry. We\'ll get back to you soon. </p><br>';
+    }
